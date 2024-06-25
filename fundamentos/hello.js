a54b84a8562cd578ca1b4d449f0deda1664a3cc4
Andrew Dantas <dantasandrew05@gmail.com>
2024-06-24 22:13:16 -0300

    descrição das alterações

--- fundamentos/hello.js
+++ fundamentos/hello.js
@@ -3,5 +3,8 @@
  * @author Andrew Dantas
  */
 
+// Uso do pacote colors
+const colors = require('colors')
+
 console.clear() //limpar a tela
-console.log("Hello World")
+console.log("Hello World".inverse.italic)
