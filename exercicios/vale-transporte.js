38ceeaf1007fb1a966b5c2599ea9c61b5d030836
Andrew Dantas <dantasandrew05@gmail.com>
2024-07-04 20:41:50 -0300

    exercícios

--- exercicios/vale-transporte.js
+++ exercicios/vale-transporte.js
@@ -4,15 +4,15 @@
  */
 
 const input = require('readline-sync')
+const vt = 6
 
 //variáveis
-let salario,vt,vtdesconto
+let salario,vtdesconto
 
 console.clear()
 console.log("Cálculo do valor de desconto do Vale-Transporte")
 //entrada
 salario = Number(input.question("Digite o valor do salário: "))
-vt = Number(input.question("Digite o valor do desconto em porcentagem: "))
 
 //processamento 1 
 valor = (vt * salario) / 100
@@ -20,7 +20,6 @@ valor = (vt * salario) / 100
 //saída 1
 console.log("_______________________________________________")
 console.log(`Valor do salário: R$ ${salario.toFixed(2)}`)
-console.log(`Desconto do Vale-Transporte: ${vt}%`)
 console.log(`Valor de desconto do Vale-Transporte: R$ ${valor.toFixed(2)}`)
 
 //processamento 2
