4f797d3803906098ea4581d6539512ad456c0778
Andrew Dantas <dantasandrew05@gmail.com>
2024-07-05 21:01:36 -0300

    javascript

---
+++ exercicios/conversor_MBparaGB.js
@@ -0,0 +1,15 @@
+/**
+ * Conversor de Mb para Gb
+ * @author Andrew Dantas
+ */
+
+const input = require('readline-sync')
+const Gb = 1.024
+
+let Mb
+
+console.clear()
+console.log("Converter Mb para Gb")
+
+Mb = Number(input.question("Digite quantos Megas"))
+
