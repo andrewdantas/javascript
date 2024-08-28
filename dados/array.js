0e4e278f9e4de5a1cf61c34cc62a188462cd2054
Andrew Dantas <dantasandrew05@gmail.com>
2024-08-28 19:16:07 -0300

    estrutura de dados

---
+++ dados/array.js
@@ -0,0 +1,18 @@
+/**
+ * Estrtura de dados
+ * Array (vetor)
+ * @author Andrew Dantas
+ */
+
+// array simples
+//              [0]             [1]         [2]         [3]
+let times = ["Corinthians", "São Paulo", "Santos", "Palmeiras"]
+console.log(times)
+console.table(times)
+console.log(times[0])
+
+// array multidimensaional (matriz)
+let boletim = [[8, 7, 9, 3], [4, 5, 8, 6]] // "linhas de uma tabela"
+console.log(boletim)
+console.table(boletim)
+console.log(boletim[0][1]) // [linha] [coluna]
